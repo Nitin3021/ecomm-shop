a05d18eb6fe12162eea0d9568e18bb510dbca473
Nitin <nitinpillai30@yahoo.com>
2021-07-26 09:32:00 +0700

    Adding middleware routes
    Adding middleware to validate backend routes. NotFound & errorHandler
    would be validating the request accordingly.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -16,5 +17,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
