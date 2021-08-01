01ff7baf65a5737d7c3c8c7b4ce9207b8d3bda05
Nitin <nitinpillai30@yahoo.com>
2021-08-01 12:16:07 +0700

    Adding user login/signup functions
    New reducer/actions have been setup for Registering new user.

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -40,9 +40,9 @@ Rating.defaultProps = {
 }
 
 Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired
+    value: PropTypes.number,
+    text: PropTypes.string,
+    color: PropTypes.string
 }
 
 export default Rating
