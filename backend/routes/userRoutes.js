c746046e444646e630be3310853a9c3f592f804c
Nitin <nitinpillai30@yahoo.com>
2021-08-02 12:28:23 +0700

    Adding new route to update user profile
    Adding route PUT /api/users/profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,6 @@ import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile) 
 
 export default router
