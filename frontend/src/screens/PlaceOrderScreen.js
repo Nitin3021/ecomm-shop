0da486d0e6484c2d6e8c4161d1459aeaf149b47a
Nitin <nitinpillai30@yahoo.com>
2021-08-04 14:05:20 +0700

    Adding Order Screen & payment endpoint
    1. Adding OrderScreen to view the last step summary page of the details.
    2. Payment route is added to backend to update the status to paid upon
       payment.

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -47,7 +47,7 @@ const PlaceOrderScreen = ({ history }) => {
     }
 
     return (
-        <div>
+        <>
             <CheckoutSteps step1 step2 step3 step4 />
             <Row>
                 <Col md={8}>
@@ -150,7 +150,7 @@ const PlaceOrderScreen = ({ history }) => {
                     </Card>
                 </Col>
             </Row>
-        </div>
+        </>
     )
 }
 
