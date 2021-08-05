9e2ce73abdad79c796e712d74c37e6c94fbd2181
Nitin <nitinpillai30@yahoo.com>
2021-08-05 19:15:15 +0700

    Adding route to frontend to delete Product
    This route will be visible to only Admin and need admin privilege to
    delete.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,17 +2,12 @@ import express from 'express'
 const router = express.Router()
 import {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
 router.route('/').get(getProducts)
-
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
