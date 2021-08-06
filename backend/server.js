e54a270133649502e34ba279414c760da7f70197
Nitin <nitinpillai30@yahoo.com>
2021-08-06 15:26:00 +0700

    Adding product review functionality
    Product can be reviewed by users by providing a Rating and Comment. This
    function is validated to be performed only 1 user per product.
    Rating will be then visible under the product and an average User's
    rating will be defined at the top of the Product item page.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -15,6 +17,10 @@ connectDB()
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
